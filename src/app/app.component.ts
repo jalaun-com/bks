653e4245a38741e65f81a4c9de6f16fd4776fa39
“dhruvpratapsingh” <dhruvpsg@gmail.com>
2025-05-15 15:41:57 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HeroComponent} from './components/landing/hero/hero.component';
+import {HeaderComponent} from './components/landing/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroComponent, HeaderComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
---
+++ src/app/components/landing/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-hero',
+  imports: [
+
+  ],
+  templateUrl: './hero.component.html',
+  standalone: true,
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+
+}
